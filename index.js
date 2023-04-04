a7ed3d3f09c1e45baaa2133c2e9774989c86a378
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-04-04 16:33:15 +0300

    8.7

--- index.js
+++ index.js
@@ -133,6 +133,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -147,7 +152,7 @@ const resolvers = {
         .filter(book => args.genre ? book.genres.includes(args.genre) : book)
     },
 
-    allAuthors: (root, args) => authors
+    allAuthors: () => authors
 
   },
   Author: {
@@ -182,6 +187,17 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
