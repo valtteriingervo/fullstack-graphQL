ea9be29ed105afdba10b2befbed37b6e503b1bd6
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-04-20 10:59:36 +0300

    8.14

--- index.js
+++ index.js
@@ -20,103 +20,6 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
-
 const typeDefs = `
   type Author {
     name: String!
@@ -162,43 +65,20 @@ const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
-
     allBooks: async (root, args) => {
-      /*
-      return books
-        .filter(book => args.author ? (book.author === args.author) : book)
+      const allBooks = await Book.find({}).populate('author')
+      // Hope this filtering is still alright for 8.14 not using the link as help
+      // (It seems to still work fine)
+      return allBooks
+        .filter(book => args.author ? (book.author.name === args.author) : book)
         .filter(book => args.genre ? book.genres.includes(args.genre) : book)
-      */
-      return Book.find({}).populate('author')
     },
-
     allAuthors: async () => {
       return Author.find({})
     }
   },
   Author: {
-    // Part1 - BookCount doesnt have to work
-    bookCount: (root) => {
-      const bookCountByAuthor = _.countBy(books.map(book => book.author))
-      const uniqueAuthors = _.keys(bookCountByAuthor)
-      const bookCounts = _.values(bookCountByAuthor)
-
-      const authorAndCountObj = uniqueAuthors.reduce(
-        (result, author, index) => {
-          result.push({
-            name: author,
-            bookCount: bookCounts[index]
-          })
-          return result
-        }, [])
-
-      return authorAndCountObj.find(authorAndBookCount => authorAndBookCount.name === root.name).bookCount
-    }
-  },
-  Book: {
-    author: (root) => {
-      return authors.find(author => author.name === root.author)
-    }
+    bookCount: async (root) => await Book.find({ author: root.id }).countDocuments()
   },
   Mutation: {
     addBook: async (root, args) => {
@@ -221,7 +101,7 @@ const resolvers = {
         })
 
         await book.save()
-        return book
+        return book.populate('author')
 
       } catch (error) {
         console.log(error)
@@ -234,16 +114,22 @@ const resolvers = {
         })
       }
     },
-    // Part1- editAuthor doesn't have to work
-    editAuthor: (root, args) => {
-      const author = authors.find(author => author.name === args.name)
-      if (!author) {
-        return null
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
+      author.born = args.setBornTo
+      try {
+        await author.save()
+      } catch (error) {
+        throw new GraphQLError('Editing number failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
       }
 
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
-      return updatedAuthor
+      return author
     }
   }
 }
